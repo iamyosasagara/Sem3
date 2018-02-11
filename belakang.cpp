c4c1c2dbde8399a8a8c779a531c379b082fa20a6
iamyosasagara <sagara.yosa22@gmail.com>
2018-02-11 12:08:04 +0700

    Antrian

---
+++ belakang.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct TNode
+{
+	int data;
+	TNode *next;
+	
+}data;
+TNode *head;
+int main(int argc, char const *argv[])
+{
+	char p;
+	int n;
+	do{
+		TNode *baru,*temp;
+		baru = new TNode;
+		cout <<"Masukkan Data = ";
+		cin >> n;
+		baru->data = n;
+		baru->next = NULL;
+		if(head==NULL)
+		{
+			head=baru;
+			head->next=NULL;
+		}
+		else
+		{
+			temp=head;
+			while(temp->next!=NULL)
+			{
+				temp=temp->next;
+			}
+			temp->next=baru;
+		}
+		
+		if (head!=NULL)
+		{
+			/* code */
+			TNode *tampil;
+			tampil = head;
+			while(tampil!=NULL)
+			{
+				cout <<tampil->data<<" ";
+				tampil = tampil->next;
+			}
+		}
+		cout << "Masih ingin menghitung (y/n) ";
+		cin >>p;
+	}while(p=='y');
+}
